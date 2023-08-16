8f8d5beb8ea3011f15a733815edd4d28b386d90a
cflin338 <cflin@wisc.edu>
2023-08-15 21:44:18 -0500

    updated player costs, created functions to scrape site for projected season totals instead of depending on csv for data, subfunctions moved into NFLReference_Scraper.py file, added boxwhisker to view range of outcomes

--- RosterConstruction.py
+++ RosterConstruction.py
@@ -39,6 +39,138 @@ import os
 import cvxpy
 import random
 import matplotlib.pyplot as plt
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+from NFLReference_Scraper import get_projections, get_advanced_stats
+
+def load_costs(data_loc = os.getcwd() + '/Data/FF_2023_Data_v2.csv', 
+               add_variability = True, 
+               prev_selected = None):
+    player_costs = pd.read_csv(data_loc)
+    player_costs['Player'] = player_costs['Player'].astype(str)
+    player_costs['Team'] = player_costs['Team'].str.upper()
+    player_costs['Cost'] = player_costs['Average ']
+    return player_costs
+
+def load_base_stats():
+    costs = load_costs()
+    qb = get_projections('QB').drop('Team', axis = 1)
+    wr = get_projections('WR').drop('Team', axis = 1)
+    rb = get_projections('RB').drop('Team', axis = 1)
+    te = get_projections('TE').drop('Team', axis = 1)
+
+    all_projections = pd.concat([qb, wr, rb, te])
+
+    costs = costs.merge(right = all_projections, how = 'left', on = 'Player')
+    for position in ['QB', 'RB', 'WR', 'TE']:
+        costs[position] = costs['Position']==position
+    
+    costs['Cost'] = costs['Cost'].fillna(0)
+    costs['Points'] = costs['Points'].fillna(0)
+    return costs
+
+def add_cost_variability(costs, prev_selected, mode = 0, variable_cost = 5):
+    #given base dataframe of players and their costs, add variability to pricing
+    #pricings will vary dependent on prev_players
+    #if mode = 0, randomly increase for all pre players at a fixed value
+    #if mode = 1, randomly increase for pre players only if selected in any previous 
+    #   iteration, more of a weight for players more selected more often.
+    #   more weight = more price raise
+    if prev_selected:
+        players_to_raise_value = costs['Player'].isin(prev_selected)
+    
+        new_cost = costs['Cost'] + [random.random()*variable_cost*i for i in players_to_raise_value]
+        costs['Cost'] = new_cost.round()
+    
+    return costs
+
+
+def optimize_team(player_info, min_qb, min_rb, min_wr, min_te, flex_positions, bench_positions, total_cost, adjust_cost=True, adjust_cost_val = 5):    
+    roster_size = min_qb + min_rb + min_wr + min_te + flex_positions + bench_positions
+    if adjust_cost: 
+        total_cost = 300 - bench_positions*adjust_cost_val
+
+    player_selection = cvxpy.Variable(shape = len(player_info), boolean = True)
+
+    # equality constraints
+    eq_constraint1 = player_info['QB'].to_numpy() @ player_selection == min_qb
+    eq_constraint2 = np.ones(len(player_info)) @ player_selection == roster_size
+
+    ineq_constraint1 = player_info['WR'].to_numpy() @ player_selection >= min_wr
+    ineq_constraint2 = player_info['RB'].to_numpy() @ player_selection >= min_rb
+    ineq_constraint3 = player_info['TE'].to_numpy() @ player_selection >= min_te
+    ineq_constraint4 = player_info['Cost'].to_numpy() @ player_selection <= total_cost - 4*3
+    
+    #optimization function
+    total_points = player_info['Points'].to_numpy() @  player_selection
+
+    #optimization problem
+    team_selection = cvxpy.Problem(cvxpy.Maximize(total_points), [eq_constraint1, eq_constraint2, ineq_constraint1, ineq_constraint2, ineq_constraint3, ineq_constraint4])
+    team_selection.solve()
+
+    return team_selection, player_info[np.round(np.abs(player_selection.value)).astype(bool)]
+
+def perform_sim(sims = 1000):
+    
+    #perform sims
+    base_stats = load_base_stats()
+    
+    counts = {}
+    totals = []
+    prev_selected = None
+    adjust_by_prev = True
+    
+    for i in range(sims):
+        #after every iteration, make players that were previously selected more expensive
+        player_info = add_cost_variability(costs=base_stats.copy(), 
+                                           prev_selected=prev_selected,
+                                           mode = 0)
+        
+        team, players = optimize_team(player_info, 
+                                      min_qb = 2, min_rb = 3, min_wr = 5, min_te = 2, 
+                                      flex_positions = 2, bench_positions = 0,#7, 
+                                      total_cost = 300-7*4, 
+                                      adjust_cost = False,)
+        if adjust_by_prev:
+            prev_selected = list(players['Player'])
+        
+        for player in players['Player']:
+            if player in counts:
+                counts[player]+=1
+            else:
+                counts[player]=1
+                
+        totals.append(team.value)
+    
+    #sort by ascending order, most appearances
+    counts = sorted(counts.items(), key = lambda x:x[1])
+
+    return counts, totals
+
+counts, totals = perform_sim(sims=1000)
+
+fig, ax = plt.subplots()
+ax.scatter(range(len(counts)), [i[1] for i in counts]) #counts.values(), )
+for idx,i in enumerate(counts):
+    ax.annotate(text = '{}, {}'.format(i[0], i[1]), 
+                xy = (idx, i[1]),
+                textcoords = 'offset points',
+                xytext = (5,0),
+                fontsize = 'small',
+                rotation='vertical')
+plt.grid()
+plt.title('Number of Sims a Player is Selected as Part of Optimal Team')
+plt.ylabel('# Appearances')
+plt.show()
+
+fig2, ax2 = plt.subplots()
+ax2.boxplot(totals, vert = False, )
+plt.title('Range of Team Total Outcomes for Simulations')
+plt.xlabel('Total Points')
+plt.show()
+"""
+previous data import code below:
 
 def load_data(data_loc = os.getcwd() + '/Data/FF_2023_Data.csv', add_variability = False, random_cost = 5, prev_selected = None):    
     # import data
@@ -67,34 +199,6 @@ def load_data(data_loc = os.getcwd() + '/Data/FF_2023_Data.csv', add_variability
         #possibility 1: increase by weight
     return player_info
 
-
-def optimize_team(player_info, min_qb, min_rb, min_wr, min_te, flex_positions, bench_positions, total_cost, adjust_cost=True, adjust_cost_val = 5):    
-    roster_size = min_qb + min_rb + min_wr + min_te + flex_positions + bench_positions
-    if adjust_cost: 
-        total_cost = 300 - bench_positions*adjust_cost_val
-
-    player_selection = cvxpy.Variable(shape = len(player_info), boolean = True)
-
-    # equality constraints
-    eq_constraint1 = player_info['QB'].to_numpy() @ player_selection == min_qb
-    eq_constraint2 = np.ones(len(player_info)) @ player_selection == roster_size
-
-    ineq_constraint1 = player_info['WR'].to_numpy() @ player_selection >= min_wr
-    ineq_constraint2 = player_info['RB'].to_numpy() @ player_selection >= min_rb
-    ineq_constraint3 = player_info['TE'].to_numpy() @ player_selection >= min_te
-    ineq_constraint4 = player_info['Cost'].to_numpy() @ player_selection <= total_cost - 4*3
-    
-    #optimization function
-    total_points = player_info['Points'].to_numpy() @  player_selection
-
-    #optimization problem
-    team_selection = cvxpy.Problem(cvxpy.Maximize(total_points), [eq_constraint1, eq_constraint2, ineq_constraint1, ineq_constraint2, ineq_constraint3, ineq_constraint4])
-
-    team_selection.solve()
-
-    #print(player_info[np.round(np.abs(player_selection.value)).astype(bool)])
-    return team_selection, player_info[np.round(np.abs(player_selection.value)).astype(bool)]
-
 counts = {}
 totals = []
 iterations = 1000
@@ -104,8 +208,6 @@ for i in range(iterations):
     #after every iteration, include a make players that were previously selected more expensive
     player_info = load_data(add_variability = True, prev_selected=prev_selected)
 
-    #team, players = optimize_team(player_info, min_qb = 2, min_rb = 3, min_wr = 5, min_te = 2, 
-    #                             flex_positions = 2, bench_positions = 0, total_cost = 300, )
     team, players = optimize_team(player_info, 
                                   min_qb = 2, min_rb = 3, min_wr = 5, min_te = 2, 
                                   flex_positions = 2, bench_positions = 0,#7, 
@@ -142,3 +244,4 @@ plt.title('Number of Sims a Player is Selected as Part of Optimal Team')
 plt.ylabel('# Appearances')
 plt.show()
 
+"""
